3e43b60420bfafd59f7a124a9d95a7b797a4598a
LYX708194 <1356250354@qq.com>
2020-11-21 13:24:29 +0800

    2020.11.21提交

---
+++ src/leecode/time2020/eleven/Solution21.java
@@ -0,0 +1,41 @@
+package leecode.time2020.eleven;
+
+import leecode.time2020.entity.ListNode;
+
+/**
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author lyx
+ * @date 2020/11/21 13:21
+ */
+public class Solution21 {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode temp = new ListNode(0);
+        ListNode pre = temp;
+        while (l1 != null && l2 != null){
+            if (l1.val < l2.val){
+                pre.next = l1;
+                l1 = l1.next;
+            }else{
+                pre.next = l2;
+                l2 = l2.next;
+            }
+            pre = pre.next;
+        }
+        pre.next = l1 == null ? l2 : l1;
+        return temp.next;
+    }
+
+}
