6ad64f3004ec2a376fa9d58af767e03c20eb8b0a
LYX708194 <1356250354@qq.com>
2020-12-02 11:48:06 +0800

    2020.12.02提交

---
+++ src/leecode/time2020/twelve/Solution321.java
@@ -0,0 +1,146 @@
+package leecode.time2020.twelve;
+
+/**
+ * 321题  冰洁最大数
+ *
+ * 给定长度分别为 m 和 n 的两个数组，其元素由 0-9 构成，表示两个自然数各位上的数字。现在从这两个数组中选出 k (k <= m + n) 个数字拼接成一个新的数，要求从同一个数组中取出的数字保持其在原数组中的相对顺序。
+ *
+ * 求满足该条件的最大数。结果返回一个表示该最大数的长度为 k 的数组。
+ *
+ * 说明: 请尽可能地优化你算法的时间和空间复杂度。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * nums1 = [3, 4, 6, 5]
+ * nums2 = [9, 1, 2, 5, 8, 3]
+ * k = 5
+ * 输出:
+ * [9, 8, 6, 5, 3]
+ * 示例 2:
+ *
+ * 输入:
+ * nums1 = [6, 7]
+ * nums2 = [6, 0, 4]
+ * k = 5
+ * 输出:
+ * [6, 7, 6, 0, 4]
+ * 示例 3:
+ *
+ * 输入:
+ * nums1 = [3, 9]
+ * nums2 = [8, 9]
+ * k = 3
+ * 输出:
+ * [9, 8, 9]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/create-maximum-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author lyx
+ * @date 2020/12/2 10:13
+ */
+public class Solution321 {
+
+
+    /**
+     * 单调栈   https://leetcode-cn.com/problems/create-maximum-number/solution/pin-jie-zui-da-shu-by-leetcode-solution/
+     * 参考题解，讲的很清晰： https://leetcode-cn.com/problems/create-maximum-number/solution/yi-zhao-chi-bian-li-kou-si-dao-ti-ma-ma-zai-ye-b-7/
+     */
+    public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+        int m = nums1.length, n = nums2.length;
+        int[] maxSubsequence = new int[k];
+        int start = Math.max(0, k - n), end = Math.min(k, m);
+        for (int i = start; i <= end; i++) {
+            int[] subsequence1 = maxSubsequence(nums1, i);
+            int[] subsequence2 = maxSubsequence(nums2, k - i);
+            int[] curMaxSubsequence = merge(subsequence1, subsequence2);
+            if (compare(curMaxSubsequence, 0, maxSubsequence, 0) > 0) {
+                System.arraycopy(curMaxSubsequence, 0, maxSubsequence, 0, k);
+            }
+        }
+        return maxSubsequence;
+    }
+
+    /**
+     * 在数组里找到k个数的最大序列
+     * 单调栈满足从栈底到栈顶的元素单调递减，从左到右遍历数组，遍历过程中维护单调栈内的元素，需要保证遍历结束之后单调栈内的元素个数等于指定的最大子序列的长度。
+     * 遍历结束之后，将从栈底到栈顶的元素依次拼接，即得到最大子序列。
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int[] maxSubsequence(int[] nums, int k) {
+        int length = nums.length;
+        int[] stack = new int[k];
+        int top = -1;
+        int remain = length - k;
+        for (int i = 0; i < length; i++) {
+            int num = nums[i];
+            while (top >= 0 && stack[top] < num && remain > 0) {
+                top--;
+                remain--;
+            }
+            if (top < k - 1) {
+                stack[++top] = num;
+            } else {
+                remain--;
+            }
+        }
+        return stack;
+    }
+
+    /**
+     * 将两个数组合并成最大序列
+     * @param subsequence1
+     * @param subsequence2
+     * @return
+     */
+    public int[] merge(int[] subsequence1, int[] subsequence2) {
+        int x = subsequence1.length, y = subsequence2.length;
+        if (x == 0) {
+            return subsequence2;
+        }
+        if (y == 0) {
+            return subsequence1;
+        }
+        int mergeLength = x + y;
+        int[] merged = new int[mergeLength];
+        int index1 = 0, index2 = 0;
+        for (int i = 0; i < mergeLength; i++) {
+            if (compare(subsequence1, index1, subsequence2, index2) > 0) {
+                merged[i] = subsequence1[index1++];
+            } else {
+                merged[i] = subsequence2[index2++];
+            }
+        }
+        return merged;
+    }
+
+    /**
+     * 自定义比较方法。首先比较两个子序列的当前元素，如果两个当前元素不同，则选其中较大的元素作为下一个合并的元素，
+     * 否则需要比较后面的所有元素才能决定选哪个元素作为下一个合并的元素。
+     * @param subsequence1
+     * @param index1
+     * @param subsequence2
+     * @param index2
+     * @return
+     */
+    public int compare(int[] subsequence1, int index1, int[] subsequence2, int index2) {
+        int x = subsequence1.length, y = subsequence2.length;
+        while (index1 < x && index2 < y) {
+            int difference = subsequence1[index1] - subsequence2[index2];
+            if (difference != 0) {
+                return difference;
+            }
+            index1++;
+            index2++;
+        }
+        return (x - index1) - (y - index2);
+    }
+
+
+
+
+}
---
+++ src/leecode/time2020/twelve/Solution402.java
@@ -0,0 +1,74 @@
+package leecode.time2020.twelve;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ *
+ * 402 移掉 k 位数字
+ *
+ * 给定一个以字符串表示的非负整数 num，移除这个数中的 k 位数字，使得剩下的数字最小。
+ *
+ * 注意:
+ *
+ * num 的长度小于 10002 且 ≥ k。
+ * num 不会包含任何前导零。
+ * 示例 1 :
+ *
+ * 输入: num = "1432219", k = 3
+ * 输出: "1219"
+ * 解释: 移除掉三个数字 4, 3, 和 2 形成一个新的最小的数字 1219。
+ * 示例 2 :
+ *
+ * 输入: num = "10200", k = 1
+ * 输出: "200"
+ * 解释: 移掉首位的 1 剩下的数字为 200. 注意输出不能有任何前导零。
+ * 示例 3 :
+ *
+ * 输入: num = "10", k = 2
+ * 输出: "0"
+ * 解释: 从原数字移除所有的数字，剩余为空就是0。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-k-digits
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author lyx
+ * @date 2020/12/2 10:56
+ */
+public class Solution402 {
+
+
+    public String removeKdigits(String num, int k) {
+        Deque<Character> deque = new LinkedList<>();
+        for (Character c:num.toCharArray()) {
+            while (!deque.isEmpty() && c < deque.peekLast() && k > 0){
+                deque.pollLast();
+                k--;
+            }
+            deque.addLast(c);
+        }
+        while (k > 0){
+            deque.removeLast();
+            k--;
+        }
+        StringBuilder str = new StringBuilder();
+        boolean isZero = true;
+        while (!deque.isEmpty()){
+            char c = deque.pollFirst();
+            if (isZero && c == '0'){
+                continue;
+            }
+            isZero = false;
+            str.append(c);
+        }
+        return str.length() == 0 ? "0" : str.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution402 solution402 = new Solution402();
+        solution402.removeKdigits("1234567890",9);
+    }
+
+
+}
