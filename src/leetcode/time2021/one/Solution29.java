f3e9209289be53ef5824771fb1e2676d01237803
LYX708194 <1356250354@qq.com>
2021-01-10 13:58:00 +0800

    2021.01.10提交

---
+++ src/leetcode/time2021/one/Solution29.java
@@ -0,0 +1,115 @@
+package leetcode.time2021.one;
+
+/**
+ * 29 两数相除
+ * 给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
+ *
+ * 返回被除数 dividend 除以除数 divisor 得到的商。
+ *
+ * 整数除法的结果应当截去（truncate）其小数部分，例如：truncate(8.345) = 8 以及 truncate(-2.7335) = -2
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: dividend = 10, divisor = 3
+ * 输出: 3
+ * 解释: 10/3 = truncate(3.33333..) = truncate(3) = 3
+ * 示例 2:
+ *
+ * 输入: dividend = 7, divisor = -3
+ * 输出: -2
+ * 解释: 7/-3 = truncate(-2.33333..) = -2
+ *  
+ *
+ * 提示：
+ *
+ * 被除数和除数均为 32 位有符号整数。
+ * 除数不为 0。
+ * 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−231,  231 − 1]。本题中，如果除法结果溢出，则返回 231 − 1。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/divide-two-integers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author lyx
+ * @date 2021/1/10 13:33
+ */
+public class Solution29 {
+
+    /**
+     * 用左移去减。
+     * 正数边界问题比较麻烦，所以改用负数计算。
+     */
+    public int divide(int dividend, int divisor) {
+        boolean sign = (dividend > 0) ^ (divisor > 0);
+        int result = 0;
+        if(dividend>0) {
+            dividend = -dividend;
+        }
+        if(divisor>0) divisor = -divisor;
+        while(dividend <= divisor) {
+            int temp_result = -1;
+            int temp_divisor = divisor;
+            while(dividend <= (temp_divisor << 1)) {
+                if(temp_divisor <= (Integer.MIN_VALUE >> 1))break;
+                temp_result = temp_result << 1;
+                temp_divisor = temp_divisor << 1;
+            }
+            dividend = dividend - temp_divisor;
+            result += temp_result;
+        }
+        if(!sign) {
+            if(result <= Integer.MIN_VALUE) return Integer.MAX_VALUE;
+            result = - result;
+        }
+        return result;
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/divide-two-integers/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-2-4/
+     */
+    public int divide2(int dividend, int divisor) {
+        int ans = -1;
+        int sign = 1;
+        if (dividend > 0) {
+            sign = opposite(sign);
+            dividend = opposite(dividend);
+        }
+        if (divisor > 0) {
+            sign = opposite(sign);
+            divisor = opposite(divisor);
+        }
+
+        int origin_dividend = dividend;
+        int origin_divisor = divisor;
+        if (dividend > divisor) {
+            return 0;
+        }
+
+        dividend -= divisor;
+        while (divisor >= dividend) {
+            ans = ans + ans;
+            divisor += divisor;
+            dividend -= divisor;
+        }
+        //此时我们传进的是两个负数，正常情况下，它就返回正数，但我们是在用负数累加，所以要取相反数
+        int a = ans + opposite(divide(origin_dividend - divisor, origin_divisor));
+        if(a == Integer.MIN_VALUE){
+            if( sign > 0){
+                return Integer.MAX_VALUE;
+            }else{
+                return Integer.MIN_VALUE;
+            }
+        }else{
+            if(sign > 0){
+                return opposite(a);
+            }else{
+                return a;
+            }
+        }
+    }
+    public int opposite(int x) {
+        return ~x + 1;
+    }
+
+}
