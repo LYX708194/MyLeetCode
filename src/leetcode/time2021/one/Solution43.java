6583a0fbca8dfe0da8c465ae6df71da5777444f5
LYX708194 <1356250354@qq.com>
2021-01-13 13:19:57 +0800

    2021.01.13提交

---
+++ src/leetcode/time2021/one/Solution43.java
@@ -0,0 +1,98 @@
+package leetcode.time2021.one;
+
+/**
+ * 43 字符串相乘
+ * 给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
+ *
+ * 示例 1:
+ *
+ * 输入: num1 = "2", num2 = "3"
+ * 输出: "6"
+ * 示例 2:
+ *
+ * 输入: num1 = "123", num2 = "456"
+ * 输出: "56088"
+ * 说明：
+ *
+ * num1 和 num2 的长度小于110。
+ * num1 和 num2 只包含数字 0-9。
+ * num1 和 num2 均不以零开头，除非是数字 0 本身。
+ * 不能使用任何标准库的大数类型（比如 BigInteger）或直接将输入转换为整数来处理。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/multiply-strings
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author lyx
+ * @date 2021/1/13 13:00
+ */
+public class Solution43 {
+
+    /**
+     * 模拟竖式乘法
+     * @author lyx
+     * @date 2021/1/13 13:17
+     * @return
+     */
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        int m = num1.length(), n = num2.length();
+        int[] ansArr = new int[m + n];
+        for (int i = m - 1; i >= 0; i--) {
+            int x = num1.charAt(i) - '0';
+            for (int j = n - 1; j >= 0; j--) {
+                int y = num2.charAt(j) - '0';
+                ansArr[i + j + 1] += x * y;
+            }
+        }
+        for (int i = m + n - 1; i > 0; i--) {
+            ansArr[i - 1] += ansArr[i] / 10;
+            ansArr[i] %= 10;
+        }
+        int index = ansArr[0] == 0 ? 1 : 0;
+        StringBuffer ans = new StringBuffer();
+        while (index < m + n) {
+            ans.append(ansArr[index]);
+            index++;
+        }
+        return ans.toString();
+    }
+
+    /**
+     * 优化竖式乘法
+     * 乘数 num1 位数为 MM，被乘数 num2 位数为 NN， num1 x num2 结果 res 最大总位数为 M+N
+     * num1[i] x num2[j] 的结果为 tmp(位数为两位，"0x","xy"的形式)，其第一位位于 res[i+j]，第二位位于 res[i+j+1]。
+     *
+     * 作者：breezean
+     * 链接：https://leetcode-cn.com/problems/multiply-strings/solution/you-hua-ban-shu-shi-da-bai-994-by-breezean/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @author lyx
+     * @date 2021/1/13 13:18
+     * @return 
+     */
+    public String multiply2(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        int[] res = new int[num1.length() + num2.length()];
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            int n1 = num1.charAt(i) - '0';
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                int n2 = num2.charAt(j) - '0';
+                int sum = (res[i + j + 1] + n1 * n2);
+                res[i + j + 1] = sum % 10;
+                res[i + j] += sum / 10;
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < res.length; i++) {
+            if (i == 0 && res[i] == 0) continue;
+            result.append(res[i]);
+        }
+        return result.toString();
+    }
+
+}
