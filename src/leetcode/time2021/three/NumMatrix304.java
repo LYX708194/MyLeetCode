e312090f399acc150fa69d9fab5469c492ac2a11
LYX708194 <1356250354@qq.com>
2021-03-02 11:56:49 +0800

    2021.03.02提交

---
+++ src/leetcode/time2021/three/NumMatrix304.java
@@ -0,0 +1,63 @@
+package leetcode.time2021.three;
+
+/**
+ * 304 二位区域和检索-矩阵不可变
+ * 给定一个二维矩阵，计算其子矩形范围内元素的总和，该子矩阵的左上角为 (row1, col1) ，右下角为 (row2, col2) 。
+ *
+ *
+ * 上图子矩阵左上角 (row1, col1) = (2, 1) ，右下角(row2, col2) = (4, 3)，该子矩形内元素的总和为 8。
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 给定 matrix = [
+ *   [3, 0, 1, 4, 2],
+ *   [5, 6, 3, 2, 1],
+ *   [1, 2, 0, 1, 5],
+ *   [4, 1, 0, 1, 7],
+ *   [1, 0, 3, 0, 5]
+ * ]
+ *
+ * sumRegion(2, 1, 4, 3) -> 8
+ * sumRegion(1, 1, 2, 2) -> 11
+ * sumRegion(1, 2, 2, 4) -> 12
+ *  
+ *
+ * 提示：
+ *
+ * 你可以假设矩阵不可变。
+ * 会多次调用 sumRegion 方法。
+ * 你可以假设 row1 ≤ row2 且 col1 ≤ col2 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/range-sum-query-2d-immutable
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author lyx
+ * @date 2021/3/2 10:53
+ */
+public class NumMatrix304 {
+
+
+    int[][] sums;
+
+    public NumMatrix304(int[][] matrix) {
+        int m = matrix.length;
+        if(m > 0){
+            int n = matrix[0].length;
+            sums = new int[m+1][n+1];
+            for (int i = 0; i < m; i++) {
+                int sum = 0;
+                for (int j = 0; j < n; j++) {
+                    sum += matrix[i][j];
+                    sums[i+1][j+1] = sum + sums[i][j+1];
+                }
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return sums[row2+1][col2+1] - sums[row2+1][col1] - sums[row1][col2+1] + sums[row1][col1];
+    }
+
+}
