40e1b780b38901c1382e2e614d6f9aa403fabbe1
LYX708194 <1356250354@qq.com>
2021-04-05 22:53:01 +0800

    2021.04.05提交

--- src/leetcode/LCP/Solution29.java
+++ src/leetcode/LCP/Solution29.java
@@ -1,30 +1,46 @@
-package leetcode.weekMatch;
+package leetcode.LCP;
 
 import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.PriorityQueue;
 
 /**
+ * 某乐团的演出场地可视作 num * num 的二维矩阵 grid（左上角坐标为 [0,0])，每个位置站有一位成员。乐团共有 9 种乐器，乐器编号为 1~9，每位成员持有 1 个乐器。
+ *
+ * 为保证声乐混合效果，成员站位规则为：自 grid 左上角开始顺时针螺旋形向内循环以 1，2，...，9 循环重复排列。例如当 num = 5 时，站位如图所示
+ *
+ *
+ *
+ * 请返回位于场地坐标 [Xpos,Ypos] 的成员所持乐器编号。
+ *
+ * 示例 1：
+ *
+ * 输入：num = 3, Xpos = 0, Ypos = 2
+ *
+ * 输出：3
+ *
+ * 解释：
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：num = 4, Xpos = 1, Ypos = 2
+ *
+ * 输出：5
+ *
+ * 解释：
+ *
+ *
+ * 提示：
+ *
+ * 1 <= num <= 10^9
+ * 0 <= Xpos, Ypos < num
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/SNJvJP
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  * @author lyx
- * @date 2021/4/5 15:07
+ * @date 2021/4/5 22:12
  */
-public class Solution0405 {
-
-
-    public int purchasePlans(int[] nums, int target) {
-        Arrays.sort(nums);
-        int left = 0,right = nums.length-1;
-        int ans = 0;
-        while (left < right){
-            if (nums[left] + nums[right] <= target){
-                ans = (ans + right - left) % 1000000007;
-                left++;
-            }else{
-                right--;
-            }
-        }
-        return ans;
-    }
+public class Solution29 {
 
     public int orchestraLayout(int num, int xPos, int yPos) {
         int half = num / 2;
@@ -107,33 +123,5 @@ public class Solution0405 {
         }
         return cur == 0 ? 9: cur;
     }
-
-    public int magicTower(int[] nums) {
-        int n = nums.length;
-        if (Arrays.stream(nums).sum() < 0)    return -1;
-        int ans = 0;
-        long blood = 1;
-        //只保存负数
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        for (int num : nums) {
-            blood += num;
-            if (num < 0)    queue.offer(num);
-            while (blood <= 0){
-                if (queue.isEmpty()) return -1;
-                blood -= queue.poll();
-                ans++;
-            }
-        }
-        return ans;
-    }
-
-
-
-    public static void main(String[] args) {
-        Solution0405 solution0405 = new Solution0405();
-        solution0405.orchestraLayout(4,1,2);
-    }
-
-
 
 }
