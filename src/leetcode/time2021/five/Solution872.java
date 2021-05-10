f19c567b5417dbc0da7fea4c92e1046a31c929c3
LYX708194 <1356250354@qq.com>
2021-05-10 21:56:05 +0800

    2021.05.10提交

---
+++ src/leetcode/time2021/five/Solution872.java
@@ -0,0 +1,82 @@
+package leetcode.time2021.five;
+
+import leetcode.entity.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 请考虑一棵二叉树上所有的叶子，这些叶子的值按从左到右的顺序排列形成一个 叶值序列 。
+ *
+ *
+ *
+ * 举个例子，如上图所示，给定一棵叶值序列为 (6, 7, 4, 9, 8) 的树。
+ *
+ * 如果有两棵二叉树的叶值序列是相同，那么我们就认为它们是 叶相似 的。
+ *
+ * 如果给定的两个根结点分别为 root1 和 root2 的树是叶相似的，则返回 true；否则返回 false 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：root1 = [3,5,1,6,2,9,8,null,null,7,4], root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+ * 输出：true
+ * 示例 2：
+ *
+ * 输入：root1 = [1], root2 = [1]
+ * 输出：true
+ * 示例 3：
+ *
+ * 输入：root1 = [1], root2 = [2]
+ * 输出：false
+ * 示例 4：
+ *
+ * 输入：root1 = [1,2], root2 = [2,2]
+ * 输出：true
+ * 示例 5：
+ *
+ *
+ *
+ * 输入：root1 = [1,2,3], root2 = [1,3,2]
+ * 输出：false
+ *  
+ *
+ * 提示：
+ *
+ * 给定的两棵树可能会有 1 到 200 个结点。
+ * 给定的两棵树上的值介于 0 到 200 之间。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/leaf-similar-trees
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author lyx
+ * @date 2021/5/10 21:47
+ */
+public class Solution872 {
+
+
+
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> list1 = new ArrayList<>(),list2 = new ArrayList<>();
+        dfs(root1,list1);
+        dfs(root2,list2);
+        if (list1.size() != list2.size())   return false;
+        for (int i = 0; i < list1.size(); i++) {
+            if (list1.get(i) != list2.get(i))   return false;
+        }
+        return true;
+    }
+    private void dfs(TreeNode node,List<Integer> list){
+        if (node == null)   return;
+        if (node.left == null && node.right == null){
+            list.add(node.val);
+            return;
+        }
+        dfs(node.left,list);
+        dfs(node.right,list);
+    }
+
+}
