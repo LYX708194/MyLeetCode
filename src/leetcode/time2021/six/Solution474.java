1272f9eebfcc0bc88eef947fb9e56920a4462ee5
LYX708194 <1356250354@qq.com>
2021-06-06 18:05:36 +0800

    2021.06.06提交

---
+++ src/leetcode/time2021/six/Solution474.java
@@ -0,0 +1,53 @@
+package leetcode.time2021.six;
+
+/**
+ * @author lyx
+ * @date 2021/6/6 17:37
+ */
+public class Solution474 {
+
+    public int findMaxForm(String[] strs, int m, int n) {
+        int length = strs.length;
+        //前i个字符串，使用j个0和k个1的情况下最多可以得到的字符串数量
+        int[][][] dp = new int[length + 1][m + 1][n + 1];
+        for (int i = 1; i <= length; i++) {
+            int[] zerosOnes = getZerosOnes(strs[i - 1]);
+            int zeros = zerosOnes[0], ones = zerosOnes[1];
+            for (int j = 0; j <= m; j++) {
+                for (int k = 0; k <= n; k++) {
+                    dp[i][j][k] = dp[i - 1][j][k];
+                    if (j >= zeros && k >= ones) {
+                        dp[i][j][k] = Math.max(dp[i][j][k], dp[i - 1][j - zeros][k - ones] + 1);
+                    }
+                }
+            }
+        }
+        return dp[length][m][n];
+    }
+
+    public int findMaxForm2(String[] strs, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        int length = strs.length;
+        for (int i = 0; i < length; i++) {
+            int[] zerosOnes = getZerosOnes(strs[i]);
+            int zeros = zerosOnes[0], ones = zerosOnes[1];
+            for (int j = m; j >= zeros; j--) {
+                for (int k = n; k >= ones; k--) {
+                    dp[j][k] = Math.max(dp[j][k], dp[j - zeros][k - ones] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+
+    public int[] getZerosOnes(String str) {
+        int[] zerosOnes = new int[2];
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            zerosOnes[str.charAt(i) - '0']++;
+        }
+        return zerosOnes;
+    }
+
+}
