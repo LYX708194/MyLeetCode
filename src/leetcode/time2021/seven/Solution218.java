700f2d96ccf4b2edeb057cad1dcf13443439af4d
LYX708194 <1356250354@qq.com>
2021-07-13 22:37:42 +0800

    2021.07.13提交

---
+++ src/leetcode/time2021/seven/Solution218.java
@@ -0,0 +1,41 @@
+package leetcode.time2021.seven;
+
+import java.util.*;
+
+/**
+ * @author lyx
+ * @date 2021/7/13 22:26
+ */
+public class Solution218 {
+
+
+    public List<List<Integer>> getSkyline(int[][] buildings) {
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> b[1] - a[1]);
+        List<Integer> boundaries = new ArrayList<Integer>();
+        for (int[] building : buildings) {
+            boundaries.add(building[0]);
+            boundaries.add(building[1]);
+        }
+        //获得所有横坐标排序
+        Collections.sort(boundaries);
+
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        int n = buildings.length, idx = 0;
+        for (int boundary : boundaries) {
+            while (idx < n && buildings[idx][0] <= boundary) {
+                pq.offer(new int[]{buildings[idx][1], buildings[idx][2]});
+                idx++;
+            }
+            while (!pq.isEmpty() && pq.peek()[0] <= boundary) {
+                pq.poll();
+            }
+
+            int maxn = pq.isEmpty() ? 0 : pq.peek()[1];
+            if (ret.size() == 0 || maxn != ret.get(ret.size() - 1).get(1)) {
+                ret.add(Arrays.asList(boundary, maxn));
+            }
+        }
+        return ret;
+    }
+
+}
